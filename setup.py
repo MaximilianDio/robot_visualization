a0a8b7bb6bf8fc8f25a9d154b88869199d699810
aq75owes <maximilian.dio@fau.de>
2025-12-29 23:17:57 +0100

    defined setup to include urdfpy from package

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='robot_visualization',
     version='0.1.0',
-    packages=find_packages(include=['robot_visualization', 'robot_visualization.*']),
+    packages=find_packages(include=['robot_visualization', 'robot_visualization.*',
+                                    'urdfpy', 'urdfpy.*']),
     install_requires=[
-        'urdfpy',
+        # 'urdfpy',
         'pyvista',
         'numpy',
     ]
